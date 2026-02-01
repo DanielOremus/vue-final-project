5f124e40861d2562f5a12df085db93aaf27ca21d
DanielOremus <oremusd89@gmai.com>
2026-02-01 22:45:03 +0200

    chore: add mkcert plugin for HTTPS support and update dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,23 @@ import { fileURLToPath, URL } from "node:url"
 
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import mkcert from "vite-plugin-mkcert"
 import vueDevTools from "vite-plugin-vue-devtools"
 import Components from "unplugin-vue-components/vite"
 import { PrimeVueResolver } from "@primevue/auto-import-resolver"
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    https: true,
+  },
   plugins: [
     vue(),
     vueDevTools(),
     Components({
       resolvers: [PrimeVueResolver()],
     }),
+    mkcert(),
   ],
   resolve: {
     alias: {
